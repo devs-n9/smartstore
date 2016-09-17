0de3a9021e391e7b2d5eb7538edca14603a6e20a
sarakabara@icloud.com <morozov_o92@mail.ru>
2016-09-17 12:56:41 +0300

    first_settings

---
+++ resources/views/dashboard/settings/index.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Settings</title>
+    <body>
+       
+        <label>Please add title name</label>
+        <input type="text" name="title">
+        
+        <label>Add keywords for shop</label>
+        <textarea name="keywords">Keywords</textarea>
+        
+        <label>Write description</label>
+        <textarea name="description">Description</textarea>
+        
+    </body>
+    </head>
+</html>

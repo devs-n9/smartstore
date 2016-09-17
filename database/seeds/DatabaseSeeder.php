111bbc6a981b83ea9f537bfd8b51fefeb2dc01b0
Your Name <you@example.com>
2016-09-17 21:12:14 +0300

    example sedding on table STATUS

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Status;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(StatusSeeder::class);
     }
 }
+
+
+
+class StatusSeeder extends Seeder
+{
+
+    public function run()
+    {
+        DB::table('Status')->delete();
+        Status::create([
+            'status' => '1',
+        ]);
+
+        Status::create([
+            'status' => '2',
+        ]);
+
+        Status::create([
+            'status' => '3',
+        ]);
+    }
+}
+
+

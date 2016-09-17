673610bb73327414f614f5c967a0769841eff0d4
yuliaslk <yulia.kr.k@gmail.com>
2016-09-17 09:47:02 +0300

    add migration orders & change gitignore

---
+++ database/migrations/2016_09_15_175031_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('product_id');
+            $table->integer('user_id');
+            $table->integer('count');
+            $table->string('description');
+            $table->integer('delivery_id');
+            $table->integer('payments_id');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('orders');
+    }
+}

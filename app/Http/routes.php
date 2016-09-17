d672ff119133574821bb80a31d08937dd81ce8ed
Rom4yk <roman_andreev@mts.com.ua>
2016-09-17 12:56:35 +0300

    news

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,6 +12,22 @@
 */
 
 Route::get('/', 'DefaultController@index');
+Route::get('/', 'NewsController@index');
+Route::get('/news/edit', "NewsController@lists");
+Route::get('/news/{title}', "NewsController@index");
+Route::auth();
+
 
 Route::get('/dashboard', 'Dashboard\DashboardController@index');
+Route::get('/dashboard/news', 'Dashboard\NewsController@index');
+Route::get('/dashboard/news/post/create', 'Dashboard\NewsController@create');
+Route::post('/dashboard/news/post/create', 'Dashboard\NewsController@insert');
+
+Route::get('/dashboard/news/post/edit/{id}', 'Dashboard\NewsController@edit');
+Route::post('/dashboard/news/post/edit/{id}', 'Dashboard\NewsController@update');
+
+Route::get('/dashboard/news/post/delete/{title}', 'Dashboard\NewsController@delete');
+
+
+
 

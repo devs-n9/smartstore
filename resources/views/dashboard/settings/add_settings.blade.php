cf968a9286153f742c0921af6fae36b4a489ef36
YuliiaKravchenko <ylechk@inbox.ru>
2016-09-19 23:59:42 +0300

    first part for contacts and settings

---
+++ resources/views/dashboard/settings/add_settings.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts.dashboard')
+@section('content')
+
+    <title>Add settings to the web site</title>
+    <form method="post" class="form-horizontal">
+        {{ csrf_field() }}
+        
+        <h3>Change contact information</h3>
+        
+        <label for="">Phone</label>
+        <div>
+        <input type="number" name="phone">
+        </div>
+        
+        <label for="">Email</label>
+        <div>
+        <input type="email" name="email">
+        </div>
+        
+        <label for="">Address</label>
+        <div>
+        <input type="text" name="address">
+        </div>
+        
+        <h3>Correct meta information</h3>
+        
+        <label for="">Title</label>
+        <div>
+            <input type="text" name="title">
+        </div>
+        
+        <label for="">Author</label>
+        <div>
+        <input type="text" name="author">
+        </div>
+        
+        <label for="">Keywords</label>
+        <div>
+        <textarea name="keywords" cols="30" rows="10"></textarea>
+        </div>
+        
+        <label for="">Description</label>
+        <div>
+        <textarea name="description" cols="30" rows="10"></textarea>
+        </div>
+                
+        @if(!empty($errors))
+        
+        @foreach($errors as $error)
+            <p>{{ $error }}</p>
+        @endforeach
+        
+        @endif
+        <div>
+        <input type="submit" value="Save" class="form-control btn btn-success">
+        </div>
+    </form>
+
+@endsection
---
+++ resources/views/dashboard/settings/edit_settings.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.dashboard')
+@section('content')
+
+    <title>Add settings to the web site</title>
+    <form method="post" class="form-horizontal">
+        {{ csrf_field() }}
+        
+        <h3>Change contact information</h3>
+        
+        <label for="">Phone</label>
+        <div>
+        <input type="number" name="phone" value="{{ $contacts->phone }}">
+        </div>
+        
+        <label for="">Email</label>
+        <div>
+        <input type="email" name="email" value="{{ $contacts->email }}">
+        </div>
+        
+        <label for="">Address</label>
+        <div>
+        <input type="text" name="address" value="{{ $contacts->address }}">
+        </div>
+        
+        <h3>Correct meta information</h3>
+        
+        <label for="">Title</label>
+        <div>
+            <input type="text" name="title" value="{{ $settings->title }}">
+        </div>
+        
+        <label for="">Author</label>
+        <div>
+        <input type="text" name="author" value="{{ $settings->author }}">
+        </div>
+        
+        <label for="">Keywords</label>
+        <div>
+        <textarea name="keywords" cols="30" rows="10">{{ $settings->keywords }}</textarea>
+        </div>
+        
+        <label for="">Description</label>
+        <div>
+        <textarea name="description" cols="30" rows="10">{{ $settings->description }}</textarea>
+        </div>
+        
+        <div>
+        <input type="submit" value="Save" class="form-control btn btn-success">
+        </div>
+    </form>
+
+@endsection
+
+        
+        
